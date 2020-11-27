abf8178a77c478926b45c81839a6dbb8ba9ce373
AB <ab@ABs-iMac.local>
2020-11-27 16:22:32 -0500

    main content base styles

--- src/Components/mainContent/mainContent.js
+++ src/Components/mainContent/mainContent.js
@@ -19,7 +19,131 @@ class App extends Component {
                 <h6>{this.props.title}</h6>
                 <h6>{this.props.site}</h6>
             </header>
-            <h1>{this.props.site} here just for testing the full page landing component</h1>
+            <div className="maincontent--inner">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dui ex, blandit vel massa et, laoreet bibendum nibh. In eu lorem lorem. Vivamus felis neque, ultricies nec mi nec, feugiat tincidunt orci. Nunc mi nunc, tristique nec tempus eget, faucibus vel neque. Nam bibendum nibh quis augue pulvinar commodo. Nulla at convallis massa. Cras eros justo, pretium pellentesque sollicitudin sodales, mollis ac tellus. Praesent ut eleifend velit, ac vestibulum est. Vestibulum fermentum massa et iaculis suscipit. Etiam sit amet odio dolor. Integer bibendum, metus et consectetur placerat, dolor erat imperdiet diam, et cursus magna orci egestas tellus. Phasellus ornare velit et leo dictum, quis malesuada orci congue. Nullam dapibus odio vitae tortor eleifend luctus. Aenean suscipit augue suscipit auctor dictum. Integer luctus commodo rutrum.</p>
+                <div className="albumlist">
+                    <div className="albumlist--entry">
+                        <div className="albumlist--info">
+                            <div className="albumlist--info-cover">
+                                <img src="https://albertobonora.com/feeds/wp-content/uploads/2019/11/Slipknot_-_We_Are_Not_Your_Kind.png" />
+                                <span>50</span>
+                            </div>
+                            <div className="albumlist--info-details">
+                                <span className="albumlist--info-title">The Deadbeat Bang of Heartbreak City</span>
+                                <span className="albumlist--info-artist">Beach Slang</span>
+                                <ul>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="albumlist--media"></div>
+                    </div>
+                    <div className="albumlist--entry">
+                        <div className="albumlist--info">
+                            <div className="albumlist--info-cover">
+                                <img src="https://albertobonora.com/feeds/wp-content/uploads/2019/11/Sleater-Kinney_-_The_Center_Wont_Hold.jpg" />
+                                <span>50</span>
+                            </div>
+                            <div className="albumlist--info-details">
+                                <span className="albumlist--info-title">The Deadbeat Bang of Heartbreak City</span>
+                                <span className="albumlist--info-artist">Beach Slang</span>
+                                <ul>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="albumlist--media"></div>
+                    </div>
+                    <div className="albumlist--entry">
+                        <div className="albumlist--info">
+                            <div className="albumlist--info-cover">
+                                <img src="https://albertobonora.com/feeds/wp-content/uploads/2019/11/Slipknot_-_We_Are_Not_Your_Kind.png" />
+                                <span>50</span>
+                            </div>
+                            <div className="albumlist--info-details">
+                                <span className="albumlist--info-title">The Deadbeat Bang of Heartbreak City</span>
+                                <span className="albumlist--info-artist">Beach Slang</span>
+                                <ul>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="albumlist--media"></div>
+                    </div>
+                    <div className="albumlist--entry">
+                        <div className="albumlist--info">
+                            <div className="albumlist--info-cover">
+                                <img src="https://albertobonora.com/feeds/wp-content/uploads/2019/11/Slipknot_-_We_Are_Not_Your_Kind.png" />
+                                <span>50</span>
+                            </div>
+                            <div className="albumlist--info-details">
+                                <span className="albumlist--info-title">The Deadbeat Bang of Heartbreak City</span>
+                                <span className="albumlist--info-artist">Beach Slang</span>
+                                <ul>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="albumlist--media"></div>
+                    </div>
+                    <div className="albumlist--entry">
+                        <div className="albumlist--info">
+                            <div className="albumlist--info-cover">
+                                <img src="https://albertobonora.com/feeds/wp-content/uploads/2019/11/Slipknot_-_We_Are_Not_Your_Kind.png" />
+                                <span>50</span>
+                            </div>
+                            <div className="albumlist--info-details">
+                                <span className="albumlist--info-title">The Deadbeat Bang of Heartbreak City</span>
+                                <span className="albumlist--info-artist">Beach Slang</span>
+                                <ul>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Track Name</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="albumlist--media"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }
--- src/Components/playlists/playlists.js
+++ src/Components/playlists/playlists.js
@@ -1,6 +1,7 @@
 import './playlists.scss';
 
 function Playlists(props){
+    //make an array of objects that contain title, link and image path and loop through that to render the items below
     const year = new Date();
     return(
         <section className="playlists">
@@ -10,6 +11,7 @@ function Playlists(props){
                     <div className="playlists--item"><a href="#" target="_blank" rel="noopener noreferrer">2020 Singles Playlist</a></div>
                     <div className="playlists--item"><a href="https://music.youtube.com/playlist?list=RDCLAK5uy_mk6AmqcHgCRhyJuYsQz5CCVdCF4SRGivs&feature=share&playnext=1" target="_blank" rel="noopener noreferrer">YouTube Music 2020 Metal Playlist</a></div>
                     <div className="playlists--item"><a href="https://music.youtube.com/playlist?list=RDCLAK5uy_mQmPoJ7Ray-n-7pQ0gnf5Tq67RuATSKO4&feature=share&playnext=1" target="_blank" rel="noopener noreferrer">YouTube Music 2020 Punk Playlist</a></div>
+                    <div className="playlists--item"><a href="https://music.youtube.com/playlist?list=RDCLAK5uy_mQmPoJ7Ray-n-7pQ0gnf5Tq67RuATSKO4&feature=share&playnext=1" target="_blank" rel="noopener noreferrer">YouTube Music 2020 Rock Playlist</a></div>
                 </div>
             </div>
             <footer>
