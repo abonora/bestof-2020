a80074e1837e9848e7e88fc0489cdd6661ad899a
AB <ab@ABs-iMac.local>
2020-12-14 16:42:09 -0500

    clean up

--- src/Components/playlists/playlists.js
+++ src/Components/playlists/playlists.js
@@ -2,15 +2,12 @@ import './playlists.scss';
 import Lazyload from 'react-lazyload';
 
 function Playlists(props){
-    //make an array of objects that contain title, link and image path and loop through that to render the items below
-    console.log(props.playlist);
     const year = new Date();
     return(
         <section className="playlists">
             <div className="playlists--inner">
                 <h5 className="playlists--title">Also Check These Out</h5>
                 <div className="playlists--scroll">
-                    {/* <div className="playlists--item"><a href="https://www.youtube.com/playlist?list=PLU8FrQ7tP8P7YAgfg-OAYWPdkmrUpRP12" target="_blank" rel="noopener noreferrer"><Lazyload once><img src="./../../playlist-singles.jpg" title="2020 Singles Playlist" alt="2020 Singles Playlist" /></Lazyload></a></div> */}
                     {
                         props.playlist.map((item,index) => (
                             <div className="playlists--item" key={index}>
